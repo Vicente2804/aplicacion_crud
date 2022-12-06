941167148673df05a4c33f0687366f31f28a0460
vicente Valencia <varv280496@gmail.com>
2022-12-05 22:54:32 -0600

    agrege solo unos pocos comentarios

--- index.js
+++ index.js
@@ -6,30 +6,30 @@ const objEmpleado = {
     puesto: ''
 }
 
-let editando = false;
+let editando = false;   //esto no lo comprendo muy bien, solo se que le estoy asignando un buleano en false 
 
 const formulario = document.getElementById('formulario');
 const nombreImput = document.getElementById('nombre');
-const puestoImput = document.getElementById('puesto');
+const puestoImput = document.getElementById('puesto');       // todas estas variables les estoy asignando un valor 
 const btnAgregar = document.getElementById('btnAgregar')
 
 formulario.addEventListener('submit' ,validarFormulario);
 
 function validarFormulario (e) {
-    e.preventDefault();
+    e.preventDefault();  // esta funcion no la comprendo todavia
 
-    if(nombreImput.value === '' || puestoImput.value === ''){
-       alert('Todos los campos deven de ser llenados')
+    if(nombreImput.value === '' || puestoImput.value === ''){   //si el valor de nombreImput es igual a un string vacio o el valor de puestoImput es igual a un string vacio,
+       alert('Todos los campos deven de ser llenados')      //nos mandara esta alerta
        return;
     }
 
     if(editando){
-        editarEmpleado()
+        editarEmpleado()    // esto no lo comprendo
         editando = false;
     }
     else {
-        objEmpleado.id = Date.now();
-        objEmpleado.nombre = nombreImput.value;
+        objEmpleado.id = Date.now(); // date.now no se que sea o para que sirva
+        objEmpleado.nombre = nombreImput.value; // aqui agregamos los valores de los imputs al objeto 
         objEmpleado.puesto = puestoImput.value;
 
         agregarEmpleado(objEmpleado);
@@ -38,19 +38,19 @@ function validarFormulario (e) {
 }
 
 
-function agregarEmpleado(empleado) {
-    let baseDatos =JSON.parse(localStorage.getItem('cliente'));
-    baseDatos.push(empleado)
-    localStorage.setItem('cliente' , JSON.stringify(baseDatos));
+function agregarEmpleado(empleado) {   // creo que empleado es una variable que se va acrear
+    let baseDatos =JSON.parse(localStorage.getItem('cliente')); //con getItem traemos un string de la llave cliente que creamos en el localStorage y con el JSON.parse lo convertimos en array y todos esos datos los guardamos en la variable baseDatos
+    baseDatos.push(empleado) //los valores guardados en baseDatos los agregamos a la variable empleado
+    localStorage.setItem('cliente' , JSON.stringify(baseDatos));//con localStorage.setItem agregamos los datos guardados en baseDatos y el JSON.stringify lo convierte en arreglo
     mostrarEmpleados();
 
-    formulario.reset();
+    formulario.reset(); //limpiamos los imputs con el .reset
 
     limpiarObjeto();
 }
 
 function limpiarObjeto() {
-    objEmpleado.id = '';
+    objEmpleado.id = '';    // aqui solo estamos asignandoles strings vacios como nuevos valores al objEmpleado
     objEmpleado.nombre = '';
     objEmpleado.puesto = '';
 }
